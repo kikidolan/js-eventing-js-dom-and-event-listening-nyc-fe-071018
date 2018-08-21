6da4f4013d5ab4ecc0f9e79f30c255ec307e4b46
Kiki Dolan <cdolan323@gmail.com>
2018-08-21 23:35:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const main = document.querySelector('#main')
 const paragraph = document.querySelector('p')
 
+let divs = document.querySelectorAll('div')
+
+function bubble(event) {
+  console.log(this.firstChild.nodeValue.trim() + 'bubbled')
+}
+
 main.addEventListener('click', (event) => {
   alert('Nice click!')
 })
